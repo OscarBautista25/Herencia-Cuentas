0c250fa9b60505758c9d290d5b6b076e87cd5f19
Oscar Bautista <oscar.bautista@correounivalle.edu.co>
2023-09-06 19:29:18 -0500

    metodo retirar

--- Cuenta.java
+++ Cuenta.java
@@ -12,6 +12,19 @@ public class Cuenta {
         this.saldo = saldo;
         this.tasaAnual = tasaAnual;
     }
+    public void consignar(float cantidad){
+        saldo = saldo + cantidad;
+    }
+
+    public void retirar(float cantidad){
+        saldo=saldo-cantidad;
+        if (saldo>=cantidad) {
+            saldo=saldo-cantidad;
+        } else {
+            System.out.println("No tiene saldo suficiente");
+        }
+    }
+
 
     
 }
